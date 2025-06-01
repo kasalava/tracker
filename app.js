66f608d6daa6225ed12d8dc4a12900189eed54ca
kasalava <kasalava86@gmail.com>
2025-06-01 23:00:40 +0500

    close modal window

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const modal = document.querySelector('#modal')//точка при классе, решетка при id
 const content = document.querySelector('#content')
+const backdrop = document.querySelector('#backdrop')
 
 content.addEventListener('click', openCard)
+backdrop.addEventListener('click', closeModal)
 
 modal.classList.add('open')
 
 setTimeout(function(){
-
+modal.classList.remove('open') 
 }, 2000)//задержка 2000мс
 
 function openCard(){
 
-    console.log('click is working')
+    modal.classList.add('open')
 }
 
-console.log('modal', modal)
+function closeModal(){
+    modal.classList.remove('open')//закрываем модальное окно
+}
