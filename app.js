bfaae754e806f05d1f7216997bee4efeaadaa59f
kasalava <kasalava86@gmail.com>
2025-06-01 22:50:46 +0500

    add setTimeout

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const content = document.querySelector('#content')
 
 content.addEventListener('click', openCard)
 
+modal.classList.add('open')
+
+setTimeout(function(){
+
+}, 2000)//задержка 2000мс
+
 function openCard(){
+
     console.log('click is working')
 }
 
