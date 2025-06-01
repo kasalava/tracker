1f4b6cc3f0162bdf602c4241b1fe32a0e45c882d
kasalava <kasalava86@gmail.com>
2025-06-01 22:46:48 +0500

    click is working

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const modal = document.querySelector()
+const modal = document.querySelector('#modal')//точка при классе, решетка при id
+const content = document.querySelector('#content')
+
+content.addEventListener('click', openCard)
+
+function openCard(){
+    console.log('click is working')
+}
+
+console.log('modal', modal)
